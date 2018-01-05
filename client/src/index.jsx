151f99d8f9baee94a5fd325dcb1d57d5216a3e61
Ofir Zisman <zismanofir@gmail.com>
2018-01-05 13:08:39 -0800

    Add initial display list

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -20,6 +20,10 @@ class App extends React.Component {
     this.getTopVoted = this.getTopVoted.bind(this);
   }
 
+  componentDidMount() {
+   this.displayShowList(); 
+  }
+
   searchShow(showToSearch) {
   	$.ajax({
   		url: '/search',
