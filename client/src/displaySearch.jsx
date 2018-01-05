938a2ce230508f583d65f1f85707ed10ace2447c
Ofir Zisman <zismanofir@gmail.com>
2018-01-05 10:16:06 -0800

    Displays search results

---
+++ client/src/displaySearch.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+var SearchDisplay = (props) => {
+	return (
+		<div>
+			<div>Search Result</div>
+			<div>
+				<div>Name: {props.show.name}</div>
+				<img src={`http://image.tmdb.org/t/p/w185//${props.show.poster_path}`}></img>
+				<div>Overview: {props.show.overview}</div>
+				<div>Rating: {props.show.vote_average}</div>
+				<button onClick={(e) => props.addToList(show)}>Add to List</button>
+			</div>	
+		</div>
+	);	
+}
+
+export default SearchDisplay;
