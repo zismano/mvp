4136078c96c728dca576c658271eef09c84593c3
Ofir Zisman <zismanofir@gmail.com>
2018-01-05 10:16:27 -0800

    Add add to DB function

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,50 +8,65 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      shows: []
+      shows: [],
+      search: []
     }
 
     this.searchShow = this.searchShow.bind(this);
+    this.addToList = this.addToList.bind(this);
   }
 
   searchShow(showToSearch) {
   	$.ajax({
-  		url: '/shows',
+  		url: '/search',
   		type: "GET",
   		data: {title: showToSearch},
   		success: (show) => {
   			console.log('success', show);
-        console.log(show);
+        let search = [show];
+        console.log(search);
+        console.log(this);
+        this.setState({
+          search: [show]
+        })
   		},
   		error: (err) => {
-        console.log(JSON.stringify(err));
-
-        console.log('ERROR');
   			console.log(err);
   		}
   	})
   }
 
+  addToList(show) {
+    $.ajax({
+      url: '/shows',
+      type: "POST",
+      data: show,
+      success: (message) => {
+        //get request with all shows from db
+        console.log(`Success ${message}`);
+      },
+      error: (err) => {
+        console.log(`Error ${err}`);
+      }
+    })
+  }
+
   render() {
     return (
     	<div>
-   		<h1>Welcome to My TV-Show List</h1>
-		<div>
-			<h2>My TV-Show list</h2>
-			<li>Game of thrones</li>
-			<li>Game of thrones</li>
-			<li>Game of thrones</li>
-			<li>Game of thrones</li>
-			<li>Game of thrones</li>
-		</div>
-			<Search searchShow={this.searchShow} />
-		<div>
-			<a href="">Top 25 by popularity</a>	
-		</div>
-		<form>
-			Pick a genre
-
-		</form>
+   		 <h1>Welcome to My TV-Show List</h1>
+  		<div>
+  			<h2>My TV-Show list</h2>
+  			<li>Game of thrones</li>
+  			<li>Game of thrones</li>
+  			<li>Game of thrones</li>
+  			<li>Game of thrones</li>
+  			<li>Game of thrones</li>
+  		</div>
+			<Search 
+        searchShow={this.searchShow} 
+        displaySearch={this.state.search} 
+        addToList={this.addToList}/>
 		</div>
     );
   }
